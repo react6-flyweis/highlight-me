58b432cd3728d56d89b0cf3c826a026de540955e
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-27 19:59:40 +0530

    feat: implement routing in App component with DashboardLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Route, Routes } from "react-router";
+import DashboardLayout from "./components/layouts/DashboardLayout";
+import { authRoutes, routes } from "./routes";
+
 function App() {
-  return <>dsjfkdsfk</>;
+  return (
+    <Routes>
+      {/* <Route element={<AuthLayout />}> */}
+      {authRoutes.map((route) => (
+        <Route element={route.element} key={route.path} path={route.path} />
+      ))}
+      {/* </Route> */}
+
+      {/* Define your routes here */}
+      <Route element={<DashboardLayout />}>
+        {routes.map((route) => (
+          <Route element={route.element} key={route.path} path={route.path} />
+        ))}
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
