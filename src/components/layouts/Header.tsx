ab4ed70ab3b19ccb645bb98500efa71144e42264
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-27 19:58:39 +0530

    feat: enhance Header component with additional icons and improved layout

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,4 +1,10 @@
-import { LogOutIcon, SearchIcon, UserIcon } from "lucide-react";
+import {
+  LogOutIcon,
+  SearchIcon,
+  UserIcon,
+  ChevronDownIcon,
+  BellIcon,
+} from "lucide-react";
 import { Link } from "react-router";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
@@ -12,7 +18,7 @@ import { Input } from "@/components/ui/input";
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b-2 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b-2 bg-white">
       <div className="flex h-14 items-center">
         {/* Logo/Brand */}
         <div className="flex items-center justify-center gap-2"></div>
@@ -23,12 +29,16 @@ export function Header() {
             <div className="relative ">
               <SearchIcon className="-translate-y-1/2 absolute top-1/2 left-2.5 h-4 w-4 text-muted-foreground" />
               <Input
-                className="h-12 w-60 rounded-full border pl-8"
-                placeholder="=search"
+                className="w-60 border pl-8"
+                placeholder="Search"
                 type="search"
               />
             </div>
           </div>
+
+          <Button variant="ghost" size="icon">
+            <BellIcon className="h-4 w-4 text-muted-foreground" />
+          </Button>
 
           {/* User Profile Section */}
           <nav className="flex items-center space-x-2">
@@ -36,22 +46,17 @@ export function Header() {
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button
-                  className="relative h-12 rounded-full border pl-1"
+                  className="relative pl-1 flex items-center hover:bg-transparent"
                   variant="ghost"
                 >
                   <Avatar className="size-10">
                     <AvatarImage alt="" src="/user.jpg" />
                     <AvatarFallback>AU</AvatarFallback>
                   </Avatar>
-                  <div className="ml-2 flex flex-col items-start">
-                    <span className="font-medium text-xs">Name</span>
-                    <span className="text-muted-foreground text-xs">
-                      variar@gmail.com
-                    </span>
-                    <span className="text-muted-foreground text-xs">
-                      24-05-2024
-                    </span>
+                  <div className="ml-1 flex flex-col items-start">
+                    <span className="font-medium text-xs">Admin</span>
                   </div>
+                  <ChevronDownIcon className="h-4 w-4 text-muted-foreground" />
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end" className="w-56" forceMount>
