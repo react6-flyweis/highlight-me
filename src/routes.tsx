dbe28a2109216d71b526c8f863f66cdc01e9f1c6
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-27 19:59:24 +0530

    feat: add initial routing setup

---
+++ src/routes.tsx
@@ -0,0 +1,36 @@
+// import { lazy } from "react";
+import type { RouteObject } from "react-router";
+
+// import NotFoundPage from "./pages/Notfound";
+
+// const HomePage = lazy(() => import("./pages/Home/Home"));
+
+export const authRoutes: RouteObject[] = [
+  {
+    path: "/login",
+    element: <>Login</>,
+  },
+  {
+    path: "/register",
+    element: <>Register</>,
+  },
+  // {
+  //   path: '/forgot-password',
+  //   element: <ForgotPasswordPage />,
+  // },
+  // {
+  //   path: '/reset-password/:token',
+  //   element: <ResetPasswordPage />,
+  // },
+];
+
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <>Home</>,
+  },
+  //   {
+  //     path: "*",
+  //     element: <NotFoundPage />,
+  //   },
+];
