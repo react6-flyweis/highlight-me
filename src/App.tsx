ba2783581ebe3ba734be398b7262f7d16669cc7e
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-28 16:13:09 +0530

    feat: wrap routes in Suspense for loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { Route, Routes } from "react-router";
+import { Suspense } from "react";
 import DashboardLayout from "./components/layouts/DashboardLayout";
 import { authRoutes, routes } from "./routes";
+import { LoadingScreen } from "./components/layouts/LoadingScreen";
 
 function App() {
   return (
-    <Routes>
-      {/* <Route element={<AuthLayout />}> */}
-      {authRoutes.map((route) => (
-        <Route element={route.element} key={route.path} path={route.path} />
-      ))}
-      {/* </Route> */}
-
-      {/* Define your routes here */}
-      <Route element={<DashboardLayout />}>
-        {routes.map((route) => (
+    <Suspense fallback={<LoadingScreen />}>
+      <Routes>
+        {/* <Route element={<AuthLayout />}> */}
+        {authRoutes.map((route) => (
           <Route element={route.element} key={route.path} path={route.path} />
         ))}
-      </Route>
-    </Routes>
+        {/* </Route> */}
+
+        {/* Define your routes here */}
+        <Route element={<DashboardLayout />}>
+          {routes.map((route) => (
+            <Route element={route.element} key={route.path} path={route.path} />
+          ))}
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
