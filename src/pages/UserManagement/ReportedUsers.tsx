998b31c5accff23ae2d5d19ffb384d72443e922a
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-08-29 20:08:35 +0530

    feat: add UsersBreadcrumb to Reported Users page

--- src/pages/UserManagement/ReportedUsers.tsx
+++ src/pages/UserManagement/ReportedUsers.tsx
@@ -2,10 +2,12 @@ import { DataTable } from "@/components/table/DataTable";
 import { PageLayout } from "@/components/layouts/PageLayout";
 import { reportedUsersColumns } from "@/components/UserManagement/reportedUsersColumns";
 import allUsersData from "@/components/UserManagement/allUsersData";
+import { UsersBreadcrumb } from "@/components/UserManagement/UsersBreadcrumb";
 
 export default function AllUsers() {
   return (
     <PageLayout title="Reported Users">
+      <UsersBreadcrumb />
       <DataTable
         columns={reportedUsersColumns}
         data={allUsersData}
