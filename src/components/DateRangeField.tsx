7fb49f047369caba0f7633045aefcb386e911ac4
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-01 19:31:15 +0530

    feat: add DateRangeField component for date selection

---
+++ src/components/DateRangeField.tsx
@@ -0,0 +1,57 @@
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useEffect, useState } from "react";
+import type { DateRange } from "react-day-picker";
+
+export function DateRangeField({
+  value,
+  onChange,
+}: {
+  value: DateRange | undefined;
+  onChange: (range: DateRange | undefined) => void;
+}) {
+  const [date, setDate] = useState<DateRange | undefined>(value);
+
+  useEffect(() => {
+    setDate(value);
+  }, [value]);
+
+  function handleSelect(range: DateRange | undefined) {
+    setDate(range);
+    onChange(range);
+  }
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          size="sm"
+          variant="outline"
+          data-empty={!date}
+          className="data-[empty=true]:text-muted-foreground w-full justify-start text-left font-normal"
+        >
+          <CalendarIcon />
+          {date && date.from && date.to ? (
+            <span className="text-xs">
+              {format(date.from, "P") + " - " + format(date.to, "P")}
+            </span>
+          ) : date && date.from ? (
+            <span className="text-xs">{format(date.from, "P")}</span>
+          ) : (
+            <span>Pick a date</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar mode="range" selected={date} onSelect={handleSelect} />
+      </PopoverContent>
+    </Popover>
+  );
+}
