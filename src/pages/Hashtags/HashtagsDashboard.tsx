56aefca6478bdf16c87a1ef30513aaf070060393
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-03 19:52:03 +0530

    feat: integrate HashtagsTrendsTable and HashtagsQuickActions into HashtagsCategoryPage

--- src/pages/Hashtags/HashtagsDashboard.tsx
+++ src/pages/Hashtags/HashtagsDashboard.tsx
@@ -1,6 +1,8 @@
 import { PageLayout } from "@/components/layouts/PageLayout";
 import { HashtagsNav } from "@/components/Hashtags/HashtagsNav";
 import { StatCard } from "@/components/StatCard";
+import { HashtagsQuickActions } from "@/components/Hashtags/HashtagsQuickActions";
+import { HashtagsTrendsTable } from "@/components/Hashtags/HashtagsTrendsTable";
 
 export default function HashtagsCategoryPage() {
   return (
@@ -38,6 +40,16 @@ export default function HashtagsCategoryPage() {
           to="/hashtags-category/management"
         />
       </section>
+
+      <div className="mt-6 grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="lg:col-span-2 bg-white border border-gray-100 rounded-lg p-4">
+          <HashtagsTrendsTable />
+        </div>
+
+        <aside className="lg:col-span-1">
+          <HashtagsQuickActions />
+        </aside>
+      </div>
     </PageLayout>
   );
 }
