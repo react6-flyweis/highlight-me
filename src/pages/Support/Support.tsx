bc707dbcc8aafac0b71b2767b8ee8b3734e82bb6
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-08 19:33:04 +0530

    feat: add Support page

---
+++ src/pages/Support/Support.tsx
@@ -0,0 +1,35 @@
+import { StatCard } from "@/components/StatCard";
+
+export default function Support() {
+  return (
+    <section className="">
+      <h1 className="text-xl font-medium my-6">Support and Feedback Center</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        <StatCard
+          title="User Feedback"
+          value="2,450"
+          sub="Current tier: Branded March"
+          iconSrc="/icons/users.svg"
+          iconAlt="users"
+        />
+
+        <StatCard
+          title="Support Requests"
+          value="2,450"
+          sub="Ends in 4 days"
+          iconSrc="/icons/message-report.svg"
+          iconAlt="requests"
+        />
+
+        <StatCard
+          title="FAQ / Help Topics"
+          value="2,450"
+          sub="This Month"
+          iconSrc="/icons/clipboard-list.svg"
+          iconAlt="faqs"
+        />
+      </div>
+    </section>
+  );
+}
